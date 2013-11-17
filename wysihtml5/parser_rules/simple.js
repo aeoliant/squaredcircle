28b3d68ed35eb1a84ad31446f1bd6ed0cc9663c5
Alex Bedley <aeoliant@hotmail.com>
2013-11-17 14:49:24 -0500

    better docs

---
+++ wysihtml5/parser_rules/simple.js
@@ -0,0 +1,27 @@
+/**
+ * Very simple basic rule set
+ *
+ * Allows
+ *    <i>, <em>, <b>, <strong>, <p>, <div>, <a href="http://foo"></a>, <br>, <span>, <ol>, <ul>, <li>
+ *
+ * For a proper documentation of the format check advanced.js
+ */
+var wysihtml5ParserRules = {
+  tags: {
+    strong: {},
+    b:      {},
+    i:      {},
+    em:     {},
+    br:     {},
+    p:      {},
+    div:    {},
+    span:   {},
+    ul:     {},
+    ol:     {},
+    li:     {},
+    h2: {},
+    h1: {},
+    a:      {}
+
+  }
+};
